63a9714aef36ed0ffd364bc9ce93f140db8a1b36
Andrew Baldock <andrewbaldock3@gmail.com>
2015-06-29 11:54:00 -0700

    requirejs-text templates pt 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ require.config({
   paths: {
     'jquery': 'vendor/jquery/dist/jquery',
     'underscore': 'vendor/underscore-amd/underscore',
+    'text': 'vendor/requirejs-text/text',
     'backbone': 'vendor/backbone-amd/backbone',
   }
 });
